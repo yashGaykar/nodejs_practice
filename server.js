855bfb929d682bb334dab9edacced48275722f1d
yashGaykar <yashgaykar44@gmail.com>
2023-02-27 15:34:39 +0530

    updates: app

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require("http");
 const app = require("./app");
 
+const { allDefinitions } = require("./api/jobs/definitions/index");
+const agenda =require("./api/jobs/index")
+
+
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+// defice the jobs
+allDefinitions(agenda);
 
 server.listen(port, () => {
 	console.log(`The server is started at port ${port}`);
